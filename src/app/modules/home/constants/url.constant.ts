bdb451e356a79c24319e8ffe3cb952c9f7206579
sureshrajana <suresh.rajana1992@gmail.com>
2023-10-02 16:59:53 +0530

    1 file modified

--- src/app/modules/home/constants/url.constant.ts
+++ src/app/modules/home/constants/url.constant.ts
@@ -1,2 +1,2 @@
 export const API_URL_PREFIX = 'https://v3.football.api-sports.io';
-export const API_KEY = 'ea5a07af088d5a9bc865522472222da7';
+export const API_KEY = '19c550ede25cac768f814f33c61606c7';
